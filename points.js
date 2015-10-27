5b0395b15782c607699f11a8c474093ba935dc04
TyPh00nCdr <Typhoon.CommanderCool@gmail.com>
2015-10-28 00:36:25 +0100

    attempting to add dynamically changing gl_PointSize

--- points.js
+++ points.js
@@ -4,6 +4,12 @@
  */
 var gl;
 
+// Specify position of the vertices
+var vertices = new Float32Array([-0.5, 0,
+                                  0.5, 0]);
+
+var size = new Float32Array([10.0]);
+
 window.onload = function init()
 {
     // Get canvas and setup webGL
@@ -12,12 +18,10 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
-    // Specify position and color of the vertices
+    // Specify color of the vertices
 
-    var vertices = new Float32Array([	-0.5, -0.5,
-        0, 0.5]);
     var colors = new Float32Array([ 1, 0, 0, 1,
-        0, 0, 1, 1]);
+                                    0, 0, 1, 1]);
 
     // Configure viewport
 
@@ -40,6 +44,7 @@ window.onload = function init()
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
+
     // Load positions into the GPU and associate shader variables
 
     var bufferId = gl.createBuffer();
@@ -50,6 +55,19 @@ window.onload = function init()
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+
+    // Load squares' size into the GPU and associate shader variables
+
+    var sizeBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, sizeBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, size, gl.STATIC_DRAW);
+
+    var vSize = gl.getAttribLocation(program, "vSize");
+    gl.vertexAttribPointer(vSize, 1, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vSize);
+
+    alert("Color var: " + vColor + " | Pos var: " + vPosition + " | Size var: " + vSize);
+
     render();
 };
 
@@ -59,10 +77,23 @@ function render()
     gl.drawArrays(gl.POINTS, 0, 2);
 }
 
-function submit()
+function process()
 {
     var size = document.getElementById("size").value;
-    console.log(size);
+    var unit = document.getElementById("unit").value;
+    var ratio = screen.width / screen.height;
 
-    return false;
+    if (unit.localeCompare("Zoll") == 0)
+    {
+        size *= 25.4;
+    }
+    else
+    {
+        size *= 10.0;
+    }
+
+    var hoehe = (size) / Math.sqrt(Math.pow(ratio, 2) + 1.0);
+    // var breite = hoehe * ratio;
+
+    var pixelPerMM = screen.height / hoehe;
 }
